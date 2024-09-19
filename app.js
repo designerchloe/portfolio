ebb35c8b1a3aeef9d5226158d6f6deab305ae962
designerchloe <96744270+designerchloe@users.noreply.github.com>
2024-09-19 18:15:35 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+//tabs
+
+const workTab = document.querySelector('a.work');
+const playTab = document.querySelector('a.play');
+const work = document.querySelector('#work');
+const play = document.querySelector('#play');
+
+play.style.display = 'none';
+
+playTab.addEventListener('click', () => {
+    work.style.display = 'none';
+    play.style.display = '';
+    playTab.classList.add('selected');
+    workTab.classList.remove('selected');
+});
+
+workTab.addEventListener('click', () => {
+    work.style.display = '';
+    play.style.display = 'none';
+    playTab.classList.remove('selected');
+    workTab.classList.add('selected');
+});
+
+//work section fade ins
+
+const elementsToFadeIn = document.querySelectorAll(".fade-in-up-on-scroll");
+if (elementsToFadeIn) {
+  window.addEventListener("scroll", function(event) {
+    elementsToFadeIn.forEach(function(element) {
+      if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
+        element.classList.add("fade-in");
+      } else {
+        element.classList.remove("fade-in");
+      }
+    });
+  });
+}
+
+
+//masonry grid
+
+const container = document.querySelector('.container');
+const columnForGap = document.querySelector('.column');
+
+function generateGrid(columns, posts) {
+    container.innerHTML = '';
+    //store all column arrays which contain the relevant posts
+    let columnWrappers = {};
+
+    //create column item array and add this to the columnWrapper object
+    for(let i = 0; i < columns; i++) {
+        columnWrappers[`column${i}`] = [];
+    };
+
+    for (let i = 0; i < posts.length; i++) {
+        const column = i % columns;
+        columnWrappers[`column${column}`].push(posts[i]);
+    }
+    
+    for (let i = 0; i < columns; i++) {
+        let columnPosts = columnWrappers[`column${i}`];
+        let column = document.createElement('div');
+        column.classList.add('column');
+        if (window.innerWidth <= 600) {
+            column.classList.add('small-gap');
+        }
+        if (window.innerWidth <= 1000) {
+            column.classList.add('medium-gap');
+        }
+
+        columnPosts.forEach(post => {
+            let postDiv = document.createElement('div');
+            postDiv.classList.add('post');
+            let image = document.createElement('img');
+            image.src = post.image;
+            let textContainer = document.createElement('div');
+            textContainer.classList.add("post-text");
+            let title = document.createElement('p');
+            title.innerHTML = post.title;
+            let date = document.createElement('p');
+            date.innerHTML = post.date;
+
+            textContainer.appendChild(title);
+            textContainer.appendChild(date);
+            postDiv.appendChild(image);
+            postDiv.appendChild(textContainer);
+            column.appendChild(postDiv);
+        })
+        container.appendChild(column);
+    }
+    
+
+};
+
+
+let previousScreenSize = window.innerWidth;
+window.addEventListener('resize', () => {
+    imageIndex = 0;
+
+    if (window.innerWidth < 600 && previousScreenSize >= 600) {
+        container.style.gap = '16px';
+        generateGrid(1, posts);
+    } else if(window.innerWidth >= 600 && window.innerWidth < 1000 && (previousScreenSize < 600 || previousScreenSize >= 1000)){
+        container.style.gap = '24px';
+        generateGrid(2, posts);
+    } else if (window.innerWidth >= 1000 && previousScreenSize < 1000) {
+        generateGrid(3, posts);
+    }
+    previousScreenSize = window.innerWidth;
+})
+
+//page load
+window.addEventListener('load', () => {
+    if (previousScreenSize < 600) {
+        container.style.gap = '16px';
+        generateGrid(1, posts);
+    } else if (previousScreenSize >= 600 && previousScreenSize < 1000){
+        container.style.gap = '24px';
+        generateGrid(2, posts);
+    } else {
+        generateGrid(3, posts);
+    }
+    previousScreenSize = window.innerWidth;
+})
+
+generateGrid(3, posts);
---
+++ items.js
@@ -0,0 +1,44 @@
+const posts = [
+    {
+        id: 1,
+        title: 'Function runAway()',
+        date: 'Sep 2024',
+        image: 'Assets/runaway-links.gif',
+    },
+    {
+        id: 2,
+        title: 'Javascript Generative Art',
+        date: 'Sep 2024',
+        image: 'Assets/javascript-painting.png',
+    },
+    {
+        id: 3,
+        title: 'Augmented Reality Sculpture',
+        date: 'Mar 2022',
+        image: 'Assets/ar-sculpture.gif',
+    },
+    {
+        id: 4,
+        title: 'Lily Scan Art',
+        date: 'Nov 2021',
+        image: 'Assets/lily-scan-art.png',
+    },
+    {
+        id: 5,
+        title: 'Drawing with Type',
+        date: 'Mar 2022',
+        image: 'Assets/drawing-with-type.gif',
+    },
+    {
+        id: 6,
+        title: 'Digital Fridge Magnet Poetry',
+        date: 'Sep 2024',
+        image: 'Assets/magnet-poetry.gif',
+    },
+    {
+        id: 7,
+        title: 'Javascript Random Colors',
+        date: 'May 2024',
+        image: 'Assets/random-colors.gif',
+    },
+];
