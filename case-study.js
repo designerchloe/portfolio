c09d259586229b3c209ae421abd3e167d1fdf31f
designerChloe <chloe.allard72@gmail.com>
2024-09-25 22:07:14 -0400

    Solve arrow functionality

--- case-study.js
+++ case-study.js
@@ -1,36 +1,46 @@
-//anchor link arrows
+//arrows 
 
-// const contextPos = document.querySelector('#context').getBoundingClientRect();
-// console.log(contextPos);
-
-
-// function arrows() {
-//     if (contextPos.top >= 0 && contextPos.bottom <= window.innerHeight) {
-//         const arrow = document.querySelector('#a-context');
-//         arrow.style.opacity = 1;
-//     };
-// }
-
-// window.addEventListener('scroll', arrows);
-
+const groups = ['intro', 'context', 'problem'];
 
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
+    const height = rect.height;
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top <= 300 &&
+      rect.top >= (parseInt(`-${height}`)) + 200
     );
   }
+
+  function handler() {
+    if (isElementInViewport(introElement)) {
+        introArrow.style.opacity = '1'; 
+      } else {
+        introArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(contextElement)) {
+        contextArrow.style.opacity = '1'; 
+      } else {
+        contextArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(problemElement)) {
+        problemArrow.style.opacity = '1'; 
+      } else {
+        problemArrow.style.opacity = '0'; 
+      }
+  }
   
-  const targetElement = document.getElementById('context'); 
-  const elementToShow = document.getElementById('a-context'); 
+//   const targetElement = document.getElementById('context'); 
+//   const elementToShow = document.getElementById('a-context'); 
+
+const introElement = document.getElementById('intro');
+const introArrow = document.getElementById('a-intro');
+const contextElement = document.getElementById('context');
+const contextArrow = document.getElementById('a-context');
+const problemElement = document.getElementById('problem');
+const problemArrow = document.getElementById('a-problem');
   
-  window.addEventListener('scroll', () => {
-    if (isElementInViewport(targetElement)) {
-      elementToShow.style.opacity = '1'; 
-    } else {
-      elementToShow.style.opacity = '0'; 
-    }
-  });
+  window.addEventListener('scroll', handler);
+
+  
