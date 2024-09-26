c0be4c35aa8622979ea28c70b32fb396ef8edede
designerChloe <chloe.allard72@gmail.com>
2024-09-25 22:15:11 -0400

    Add other arrow elements

--- case-study.js
+++ case-study.js
@@ -1,7 +1,5 @@
 //arrows 
 
-const groups = ['intro', 'context', 'problem'];
-
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     const height = rect.height;
@@ -28,7 +26,43 @@ function isElementInViewport(el) {
         problemArrow.style.opacity = '1'; 
       } else {
         problemArrow.style.opacity = '0'; 
-      }
+      };
+
+    if (isElementInViewport(processElement)) {
+        processArrow.style.opacity = '1'; 
+      } else {
+        processArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(solutionElement)) {
+        solutionArrow.style.opacity = '1'; 
+      } else {
+        solutionArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(userTestingElement)) {
+        userTestingArrow.style.opacity = '1'; 
+      } else {
+        userTestingArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(improvementsElement)) {
+        improvementsArrow.style.opacity = '1'; 
+      } else {
+        improvementsArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(finalDesignsElement)) {
+        finalDesignsArrow.style.opacity = '1'; 
+      } else {
+        finalDesignsArrow.style.opacity = '0'; 
+      };
+
+    if (isElementInViewport(theDifferenceElement)) {
+        theDifferenceArrow.style.opacity = '1'; 
+      } else {
+        theDifferenceArrow.style.opacity = '0'; 
+      };
   }
   
 //   const targetElement = document.getElementById('context'); 
@@ -40,7 +74,19 @@ const contextElement = document.getElementById('context');
 const contextArrow = document.getElementById('a-context');
 const problemElement = document.getElementById('problem');
 const problemArrow = document.getElementById('a-problem');
+const processElement = document.getElementById('process');
+const processArrow = document.getElementById('a-process');
+const solutionElement = document.getElementById('solution');
+const solutionArrow = document.getElementById('a-solution');
+const userTestingElement = document.getElementById('user-testing');
+const userTestingArrow = document.getElementById('a-user-testing');
+const improvementsElement = document.getElementById('improvements');
+const improvementsArrow = document.getElementById('a-improvements');
+const finalDesignsElement = document.getElementById('final-designs');
+const finalDesignsArrow = document.getElementById('a-final-designs');
+const theDifferenceElement = document.getElementById('the-difference');
+const theDifferenceArrow = document.getElementById('a-the-difference');
   
-  window.addEventListener('scroll', handler);
+window.addEventListener('scroll', handler);
 
   
